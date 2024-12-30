b6802cd9bc40b7abfe6b14a10ba2f0dc7c43729a
Rainer Blessing <rainer.blessing@gmail.com>
2024-12-30 14:44:42 +0100

    tmux

---
+++ lua/custom/plugins/tmux.lua
@@ -0,0 +1,23 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+-- Snacks.vim hinzufügen
+return {
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+      dashboard = { enabled = true },
+    },
+  },
+}
